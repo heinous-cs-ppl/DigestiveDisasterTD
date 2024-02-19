ec3e9ae00301950127d0056c4e953ddb41a37f6c
Extr <embyrne02@gmail.com>
2024-02-18 23:34:35 -0500

    bullets are destroyed after some amount of time from when they are fired
    bullet lifetime can be set in the bullet prefab

--- Assets/Scripts/TurretScript/Bullets.cs
+++ Assets/Scripts/TurretScript/Bullets.cs
@@ -12,18 +12,27 @@ public class Bullet : MonoBehaviour
     [Header("Attributes")]
     [SerializeField] private float bulletSpeed = 5f; 
     [SerializeField] private int bulletDamage = 1;
+    [SerializeField] private float bulletLifetime = 1;
     private Transform target;
 
+    private float curLifetime = 0;
+
+    private Vector2 direction;
 
     public void SetTarget(Transform _target){
         target = _target;
     }
     private void FixedUpdate() {
-        if (!target) return;
+        if (target) {
+            direction = (target.position - transform.position).normalized; 
+        }
 
-        Vector2 direction = (target.position - transform.position).normalized; 
-
-        rb.velocity = direction * bulletSpeed; 
+        rb.velocity = direction * bulletSpeed;
+        
+        curLifetime += Time.fixedDeltaTime;
+        if(curLifetime >= bulletLifetime) {
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other){
