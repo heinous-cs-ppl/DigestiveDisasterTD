8d3e6ebf401ff5c77f6e2517f4d59187a532e557
Extr <embyrne02@gmail.com>
2024-02-20 23:51:13 -0500

    Restricted the placement of turrets to the map, clicking off the map disables placing mode

--- Assets/StudentPlacement.cs
+++ Assets/StudentPlacement.cs
@@ -6,6 +6,14 @@ public class StudentPlacement : MonoBehaviour
     [SerializeField] private Sprite studentSprite;
     private GameObject studentPreview;
     private bool canPlace = false;
+    private GameObject map;
+    private Bounds mapBounds;
+
+    void Start() {
+        map = GameObject.Find("Map");
+        SpriteRenderer mapSprite = map.GetComponent<SpriteRenderer>();
+        mapBounds = mapSprite.bounds;
+    }
 
     void Update()
     {
@@ -17,32 +25,45 @@ public class StudentPlacement : MonoBehaviour
             cursorPosition.x = Mathf.Ceil(cursorPosition.x) - 0.5f;
             cursorPosition.y = Mathf.Ceil(cursorPosition.y) - 0.5f;
 
-            // create the student preview if it doesn't exist
-            if(!(studentPreview)) {
-                // create a new GameObject with a sprite renderer
-                studentPreview = new GameObject("StudentPreview");
-                SpriteRenderer previewSprite = studentPreview.AddComponent<SpriteRenderer>();
+            // check if the cursor is on the map
+            if (mapBounds.Contains(cursorPosition)) {
+                // create the student preview if it doesn't exist
+                if (!(studentPreview)) {
+                    // create a new GameObject with a sprite renderer
+                    studentPreview = new GameObject("StudentPreview");
+                    SpriteRenderer previewSprite = studentPreview.AddComponent<SpriteRenderer>();
 
-                // set the sprite to the student sprite
-                previewSprite.sprite = studentSprite;
+                    // set the sprite to the student sprite
+                    previewSprite.sprite = studentSprite;
 
-                // set the opacity to 50%
-                Color preview = studentPreview.GetComponent<SpriteRenderer>().color;
-                preview.a = 0.5f; // Set alpha value to 50% opacity
-                studentPreview.GetComponent<SpriteRenderer>().color = preview;
-            }
-            // set the position of the student preview to the cursor's position
-            studentPreview.transform.position = cursorPosition;
+                    // set the opacity to 50%
+                    Color preview = studentPreview.GetComponent<SpriteRenderer>().color;
+                    preview.a = 0.5f; // Set alpha value to 50% opacity
+                    studentPreview.GetComponent<SpriteRenderer>().color = preview;
+                }
+                // set the position of the student preview to the cursor's position
+                studentPreview.transform.position = cursorPosition;
 
-            if (Input.GetMouseButtonDown(0)) {
-                // create student at cursor position on mouse click
-                Instantiate(student, cursorPosition, Quaternion.identity);
+                if (Input.GetMouseButtonDown(0)) {
+                    // create student at cursor position on mouse click
+                    Instantiate(student, cursorPosition, Quaternion.identity);
 
-                // disable placing once the student has been placed
-                canPlace = false;
+                    // disable placing once the student has been placed
+                    canPlace = false;
 
-                // destroy the preview
-                Destroy(studentPreview);
+                    // destroy the preview
+                    Destroy(studentPreview);
+                }
+            } else {
+                // if the player clicks somewhere not on the map
+                if (Input.GetMouseButtonDown(0)) {
+                    // disable placing
+                    canPlace = false;
+                    // destroy the preview if it exists
+                    if(studentPreview) {
+                        Destroy(studentPreview);
+                    }
+                }
             }
         }
     }
