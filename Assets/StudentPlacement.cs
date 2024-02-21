91158cf4c9e4d3561f191ef881982341014d4522
Extr <embyrne02@gmail.com>
2024-02-20 23:27:08 -0500

    Update StudentPlacement.cs
    Added a student preview which lets you see the student before it is placed

--- Assets/StudentPlacement.cs
+++ Assets/StudentPlacement.cs
@@ -2,25 +2,48 @@ using UnityEngine;
 
 public class StudentPlacement : MonoBehaviour
 {
-    public GameObject student;
+    [SerializeField] private GameObject student;
+    [SerializeField] private Sprite studentSprite;
+    private GameObject studentPreview;
     private bool canPlace = false;
 
     void Update()
     {
         // check for mouse click and if player can place the student
-        if (canPlace && Input.GetMouseButtonDown(0))
-        {
+        if (canPlace) {
             // get cursor position
             Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            // round the cursor position so the student always gets placed in the middle of the tile
+            // round the cursor position to the middle of the tile
             cursorPosition.x = Mathf.Ceil(cursorPosition.x) - 0.5f;
             cursorPosition.y = Mathf.Ceil(cursorPosition.y) - 0.5f;
-            // create student at cursor position
-            Instantiate(student, cursorPosition, Quaternion.identity);
 
-            // disable placing once the student has been placed
-            canPlace = false;
+            // create the student preview if it doesn't exist
+            if(!(studentPreview)) {
+                // create a new GameObject with a sprite renderer
+                studentPreview = new GameObject("StudentPreview");
+                SpriteRenderer previewSprite = studentPreview.AddComponent<SpriteRenderer>();
+
+                // set the sprite to the student sprite
+                previewSprite.sprite = studentSprite;
+
+                // set the opacity to 50%
+                Color preview = studentPreview.GetComponent<SpriteRenderer>().color;
+                preview.a = 0.5f; // Set alpha value to 50% opacity
+                studentPreview.GetComponent<SpriteRenderer>().color = preview;
+            }
+            // set the position of the student preview to the cursor's position
+            studentPreview.transform.position = cursorPosition;
+
+            if (Input.GetMouseButtonDown(0)) {
+                // create student at cursor position on mouse click
+                Instantiate(student, cursorPosition, Quaternion.identity);
+
+                // disable placing once the student has been placed
+                canPlace = false;
+
+                // destroy the preview
+                Destroy(studentPreview);
+            }
         }
     }
 
