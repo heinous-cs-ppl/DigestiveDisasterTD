dc0beeab59382f47a68b07b4c70ddc2e389bb263
Extr <embyrne02@gmail.com>
2024-02-22 20:59:46 -0500

    removed unnecessary files

--- Assets/Scripts/Student Scripts/PurifyBullets.cs
+++ Assets/Scripts/Student Scripts/PurifyBullets.cs
@@ -5,42 +5,6 @@ using UnityEngine;
 
 public class PurifyBullet : Bullets
 {
-
-    // [Header("References")]
-    // [SerializeField] private Rigidbody2D rb;
-
-    // // attributes (defined in StudentInfo.cs)
-    // private float bulletSpeed; 
-    // private int bulletDamage;
-    // private float bulletLifetime;
-
-    // private Transform target;
-
-    // private float curLifetime = 0;
-
-    // private Vector2 direction;
-
-    // public void SetAttributes(float speed, int damage, float lifetime) {
-    //     bulletSpeed = speed;
-    //     bulletDamage = damage;
-    //     bulletLifetime = lifetime;
-    // }
-    // public void SetTarget(Transform _target){
-    //     target = _target;
-    // }
-    // private void FixedUpdate() {
-    //     if (target) {
-    //         direction = (target.position - transform.position).normalized; 
-    //     }
-
-    //     rb.velocity = direction * bulletSpeed;
-        
-    //     curLifetime += Time.fixedDeltaTime;
-    //     if(curLifetime >= bulletLifetime) {
-    //         Destroy(gameObject);
-    //     }
-    // }
-
     private void OnCollisionEnter2D(Collision2D other){
         // Take health from Enemy
         other.gameObject.GetComponent<EnemyInfo>().purifyDamage(bulletDamage);
