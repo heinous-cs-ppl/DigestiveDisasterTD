c0423ce0eac39bd1d1c5a88a124f1a1f8234790c
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-04-03 03:48:44 -0400

    remove unnecessary instantiations when loading game scene

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -5,8 +5,6 @@ public class MainMenu : MonoBehaviour
     public void Play()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("UI scene");
-        PurifyManager.instance.SetMealCount(0);
-        Time.timeScale = 1;
     }
 
     public void Menu()
