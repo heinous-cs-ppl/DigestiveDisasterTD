97af8a8cc504000d89fa0e9dd77c952dfcfeed27
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-04-03 00:28:39 -0400

    implement pausing to stop time

---
+++ Assets/Scripts/Menu Scripts/Pause Menu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void Menu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Start Menu");
+    }
+}
