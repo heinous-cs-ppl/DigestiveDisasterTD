2178278e5892928d21bf577891d522b6f0e33957
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-04-05 18:21:14 -0400

    disable all buttons on pause

--- Assets/Scripts/Menu Scripts/PauseManager.cs
+++ Assets/Scripts/Menu Scripts/PauseManager.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseManager : MonoBehaviour
 {
     private float prevSpeed;
     public static bool paused = false;
+    private Button[] allButtons;
+
+    public void Start()
+    {
+        allButtons = FindObjectsOfType<Button>();
+    }
 
     public void Pause()
     {
         prevSpeed = Time.timeScale;
         paused = true;
         Time.timeScale = 0;
+        foreach (Button button in allButtons)
+        {
+            button.interactable = false;
+        }
     }
 
     public void Resume()
     {
         paused = false;
         Time.timeScale = prevSpeed;
+        foreach (Button button in allButtons)
+        {
+            button.interactable = true;
+        }
     }
 
     public void Menu()
